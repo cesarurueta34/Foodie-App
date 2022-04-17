cbcbfc79abc24b57c9936d2634bd16c6d4b23faf
cesarurueta34 <cesarurueta@utexas.edu>
2022-04-16 20:07:04 -0500

    setting up my app

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const express = require(`express`);
+const methodOverride = require(`method-override`);
 
-const express = require(`express`)
+const app = express();
+
+
+app.set("view engine" , "hbs");
+app.use(methodOverride(`_method`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+
+
+app.get(`/` , (req, res) => {
+  res.send(`I am working`)
+})
+
+app.listen(5001, () => {
+    console.log(`listening on port 5000`)
+})
